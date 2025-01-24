80a71d1ace445776f569fb6b2819704feddfe760
Dmitrii Sapegin <lejeboka24@gmail.com>
2025-01-24 18:40:41 +0100

    fix(): use group instead of .name check

--- CameraTrigger.gd
+++ CameraTrigger.gd
@@ -3,11 +3,11 @@ extends Area3D
 var isTrigger = false
 
 func enterTrigger(body):
-	if body.name == 'Player':
+	if body.is_in_group('player'):
 		isTrigger = true
 
 func exitTrigger(body):
-	if body.name == 'Player':
+	if body.is_in_group('player'):
 		isTrigger = false
 
 func _process(_delta: float) -> void:
